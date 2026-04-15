cb3190a93aa91624bc9f04345b72defa142b0ac6
roshanthapamagar615-dotcom <roshanthapamagar615@gmail.com>
2026-04-15 21:09:35 +0545

    Add player_character.gd with network synchronization for vehicle interactions

---
+++ player_character.gd
@@ -0,0 +1,12 @@
+extends KinematicBody2D
+
+# RPC methods for vehicle interactions
+rpc func enter_vehicle(vehicle):
+    # Code to enter the vehicle
+    pass
+
+rpc func exit_vehicle():
+    # Code to exit the vehicle
+    pass
+
+# Additional logic for player character
